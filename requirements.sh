24bec7130275f101e6648f126b59bd08e7be37bf
rwarnking <rwarnking@gmail.com>
2022-02-02 12:21:16 +0100

    Test 42

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo "Installing dependencies (default packages)!"
 
 # tlmgr install xcolor
@@ -21,61 +21,61 @@ Since it is not a default package it must be downloaded manually or by this scri
 The downloaded .zip must be unziped and compiled afterwards to be useable.
 Each of these actions will require a confirmation by you."
 
-FILE="acrotex.zip"
-if [[ -f $FILE ]]; then
-    echo "Acrotex.zip already found: no need to download!"
-else
-    # read -p "Do you want to download the acrotex.zip? " -n 1 -r
-    # echo
-    # if [[ $REPLY =~ ^[Yy]$ ]]
-    # then
-        # Source file location
-        source="http://www.math.uakron.edu/~dpstory/acrotex/acrotex.zip"
-        # Destination to save the file
-        destination=".\acrotex.zip"
-        # Download the file
-        wget $source -O $destination
-    # else
-    #     echo "Finished installation! Did not download the acrotex package!
-    #     You can rerun this script if you want to install it later."
-    #     exec $SHELL
-    #     exit
-    # fi
-fi
-
-FILE="acrotex/acrotex.ins"
-if [[ -f $FILE ]]; then
-    echo "Acrotex folder already found: skip unzip!"
-else
-    # read -p "Do you want to unzip the acrotex.zip " -n 1 -r
-    # echo
-    # if [[ $REPLY =~ ^[Yy]$ ]]
-    # then
-        echo "Unzip"
-        # Unzip
-        unzip acrotex.zip
-    # else
-    #     echo "Finished installation! Did not unzip the acrotex.zip!
-    #     You can rerun this script if you want to unzip it later."
-    #     exec $SHELL
-    #     exit
-    # fi
-fi
-
-# read -p "Do you want to compile the acrotex.ins " -n 1 -r
-# echo
-# if [[ $REPLY =~ ^[Yy]$ ]]
-# then
-    # Compile acrotex to obtain insdljs package
-    cd acrotex
-    tex acrotex.ins
-    cd ..
-
-    echo "Complete installation finished! Acrotex package should be available."
+# FILE="acrotex.zip"
+# if [[ -f $FILE ]]; then
+#     echo "Acrotex.zip already found: no need to download!"
 # else
-#     echo "Finished installation! Did not compile the acrotex.ins!
-#     You can rerun this script if you want to unzip it later."
+#     # read -p "Do you want to download the acrotex.zip? " -n 1 -r
+#     # echo
+#     # if [[ $REPLY =~ ^[Yy]$ ]]
+#     # then
+#         # Source file location
+#         source="http://www.math.uakron.edu/~dpstory/acrotex/acrotex.zip"
+#         # Destination to save the file
+#         destination=".\acrotex.zip"
+#         # Download the file
+#         wget $source -O $destination
+#     # else
+#     #     echo "Finished installation! Did not download the acrotex package!
+#     #     You can rerun this script if you want to install it later."
+#     #     exec $SHELL
+#     #     exit
+#     # fi
 # fi
-# exec $SHELL
 
-ls -R
+# FILE="acrotex/acrotex.ins"
+# if [[ -f $FILE ]]; then
+#     echo "Acrotex folder already found: skip unzip!"
+# else
+#     # read -p "Do you want to unzip the acrotex.zip " -n 1 -r
+#     # echo
+#     # if [[ $REPLY =~ ^[Yy]$ ]]
+#     # then
+#         echo "Unzip"
+#         # Unzip
+#         unzip acrotex.zip
+#     # else
+#     #     echo "Finished installation! Did not unzip the acrotex.zip!
+#     #     You can rerun this script if you want to unzip it later."
+#     #     exec $SHELL
+#     #     exit
+#     # fi
+# fi
+
+# # read -p "Do you want to compile the acrotex.ins " -n 1 -r
+# # echo
+# # if [[ $REPLY =~ ^[Yy]$ ]]
+# # then
+#     # Compile acrotex to obtain insdljs package
+#     cd acrotex
+#     tex acrotex.ins
+#     cd ..
+
+#     echo "Complete installation finished! Acrotex package should be available."
+# # else
+# #     echo "Finished installation! Did not compile the acrotex.ins!
+# #     You can rerun this script if you want to unzip it later."
+# # fi
+# # exec $SHELL
+
+# ls -R
